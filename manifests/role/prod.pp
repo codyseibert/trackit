48adae07b0b9bba99933e950a94f361e14424527
Cody Seibert <cseibert113@gmail.com>
2016-02-20 21:03:42 -0500

    updating to have puppet related code and vagrant

---
+++ manifests/role/prod.pp
@@ -0,0 +1,5 @@
+class trackit::role::prod {
+  include trackit::profile::ui
+  include trackit::profile::api
+  include trackit::profile::db
+}
---
+++ provision.sh
@@ -0,0 +1,6 @@
+# File: provision.sh
+#!/bin/bash
+curl -sL https://rpm.nodesource.com/setup_5.x | bash -
+yum install -y nodejs rpmbuild git ruby
+npm install -g grunt-cli bower
+gem install sass
